e9b91cb92e0be45a998164c9a684b13e74c3c1e0
Pedrohenrique053010@gmail.com <Pedrohenrique053010@gmail.com>
2022-09-23 22:43:32 -0300

    adiciona a solução do exercicio Independencia finaceira

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.Write("Insira seu gasto mensal: ");
+
+decimal gastos = Convert.ToDecimal(Console.ReadLine());
+
+System.Console.Write("Insira a taxa de juros mensal do investimento (%): ");
+
+decimal rendimentos = (Convert.ToDecimal(Console.ReadLine())/100);
+
+decimal investimento = Convert.ToDecimal(gastos/rendimentos);
+Console.ForegroundColor = ConsoleColor.Red;
+System.Console.Write($"Para receber {gastos} ");
+System.Console.Write($"por mês com rendimentos de {rendimentos*100:N2}% ");
+System.Console.WriteLine($"você precisa ter investido {investimento:C}");
+Console.ResetColor();
+
+System.Console.WriteLine();
+System.Console.Write("Pressione uma tecla para fechar...");
+Console.ReadKey();
